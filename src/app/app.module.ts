9c2189b87a54ecdbd7cbbac5a59f306f0e1783b4
zzyeah <541767316@qq.com>
2021-07-14 18:16:50 +0800

    project initation

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
